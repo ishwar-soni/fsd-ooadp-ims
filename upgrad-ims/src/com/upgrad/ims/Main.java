bc14c907a569e9135cdfda1990feafd2cf3deca1
Ishwar Soni <ishwar.soni@upgrad.com>
2020-10-22 08:42:47 +0530

    creating Product objects

--- upgrad-ims/src/com/upgrad/ims/Main.java
+++ upgrad-ims/src/com/upgrad/ims/Main.java
@@ -3,6 +3,20 @@ package com.upgrad.ims;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Product appleMacBookAir = new Product();
+
+        appleMacBookAir.id = 1;
+        appleMacBookAir.name = "Apple MacBook Air";
+        appleMacBookAir.category = "Laptop";
+        appleMacBookAir.salesPrice = 80000f;
+        appleMacBookAir.cost = 60000f;
+        appleMacBookAir.quantity = 1000;
+        appleMacBookAir.active = true;
+
+        System.out.println(appleMacBookAir.name);
+        System.out.println(appleMacBookAir.active);
+        appleMacBookAir.deactivate();
+        System.out.println(appleMacBookAir.active);
+        System.out.println(appleMacBookAir.getProfitOrLoss());
     }
 }
