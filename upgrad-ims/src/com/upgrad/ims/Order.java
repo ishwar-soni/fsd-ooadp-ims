5639aa145f90ab70e0784e8307ba7d639885778c
Ishwar Soni <ishwar.soni@upgrad.com>
2020-10-28 14:09:11 +0530

    hands-on encapsulating Order class

--- upgrad-ims/src/com/upgrad/ims/Order.java
+++ upgrad-ims/src/com/upgrad/ims/Order.java
@@ -1,15 +1,15 @@
 package com.upgrad.ims;
 
 public class Order {
-    int id;
+    private int id;
 
-    Vendor vendor;
-    String date;
-    Product orderedProduct;
-    int orderedQuantity;
-    float amountPaid;
+    private Vendor vendor;
+    private String date;
+    private Product orderedProduct;
+    private int orderedQuantity;
+    private float amountPaid;
 
-    Order(int id, Vendor vendor, String date, Product orderedProduct, int orderedQuantity, float amountPaid) {
+    public Order(int id, Vendor vendor, String date, Product orderedProduct, int orderedQuantity, float amountPaid) {
         this.id = id;
         this.vendor = vendor;
         this.date = date;
@@ -17,6 +17,58 @@ public class Order {
         this.orderedQuantity = orderedQuantity;
         this.amountPaid = amountPaid;
 
+        this.updateVendorCredit();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Vendor getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(Vendor vendor) {
+        this.vendor.credit -= (orderedProduct.getCost() * orderedQuantity - amountPaid);
+        this.vendor = vendor;
+        this.updateVendorCredit();
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Product getOrderedProduct() {
+        return orderedProduct;
+    }
+
+    public void setOrderedProduct(Product orderedProduct) {
+        this.vendor.credit -= (orderedProduct.getCost() * orderedQuantity - amountPaid);
+        this.orderedProduct = orderedProduct;
+        this.updateVendorCredit();
+    }
+
+    public int getOrderedQuantity() {
+        return orderedQuantity;
+    }
+
+    public void setOrderedQuantity(int orderedQuantity) {
+        this.vendor.credit -= (orderedProduct.getCost() * orderedQuantity - amountPaid);
+        this.orderedQuantity = orderedQuantity;
+        this.updateVendorCredit();
+    }
+
+    public float getAmountPaid() {
+        return amountPaid;
+    }
+
+    public void setAmountPaid(float amountPaid) {
+        this.vendor.credit -= (orderedProduct.getCost() * orderedQuantity - amountPaid);
+        this.amountPaid = amountPaid;
         this.updateVendorCredit();
     }
 
